a1415e1a489be0faaa2e78d30d9f9f0b5d32e9f0
Lui Rocha <ibecobr@gmail.com>
2020-10-10 11:04:13 -0300

    feat: better environmental variable missing handle, source-map-support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'source-map-support/register';
 // Init dababase
 import './database/init';
 // init telegram bot and controllers
@@ -5,11 +6,21 @@ import './services/init-controllers';
 import { isProd } from './helpers';
 
 // Check if any environmental variable is missing
-if (
-  !process.env.BOT_TOKEN ||
-  !process.env.CONNECTION_STRING ||
-  !process.env.ADMINS ||
-  (isProd && !process.env.HEROKU_URL)
-) {
-  throw Error('Please set environment variables!');
+
+if (!process.env.BOT_TOKEN) {
+  throw Error('Please set BOT_TOKEN environmental variable!');
+}
+
+if (!process.env.CONNECTION_STRING) {
+  throw Error('Please set CONNECTION_STRING environmental variable!');
+}
+
+if (!process.env.ADMINS) {
+  throw Error('Please set ADMINS environmental variable!');
+}
+
+if (isProd) {
+  if (!process.env.HEROKU_URL) {
+    throw Error('Please set HEROKU_URL environmental variable!');
+  }
 }
