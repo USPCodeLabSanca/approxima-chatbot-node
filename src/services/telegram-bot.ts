8b3f85d743e42667adefb50d3549795e25290784
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-10-13 23:49:28 -0300

    feat: dont let heroku dyno sleep

--- src/services/telegram-bot.ts
+++ src/services/telegram-bot.ts
@@ -2,10 +2,23 @@ process.env.NTBA_FIX_319 = 1 as any;
 
 import TelegramBot from 'node-telegram-bot-api';
 import { isProd } from '../helpers';
+import Axios from 'axios';
 
 const PORT = Number(process.env.PORT!) || 3000;
 
 const botToken = process.env.BOT_TOKEN!;
+
+const pingHeroku = (herokuUrl: string) => {
+  setTimeout(() => {
+    try {
+      Axios.get(herokuUrl);
+    }
+    catch { } // eslint-disable-line
+    finally {
+      pingHeroku(herokuUrl);
+    }
+  }, 1000 * 60 * 25); // 25 min
+};
 
 // Start Telegram bot with webHook if it is prod or pooling if it is local
 let telegramBot: TelegramBot;
@@ -13,6 +26,7 @@ if (isProd) {
   const herokuUrl = process.env.HEROKU_URL!;
   telegramBot = new TelegramBot(botToken, { webHook: { port: PORT } });
   telegramBot.setWebHook(`${herokuUrl}/bot${botToken}`);
+  pingHeroku(herokuUrl);
 }
 else {
   telegramBot = new TelegramBot(botToken, { polling: true });
