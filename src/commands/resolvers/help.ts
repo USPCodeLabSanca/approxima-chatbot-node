c0cc99c395f5d898aca7cf4091169196d72e2e5b
Michelle Wingter <login+mwingter@elo7.com>
2021-03-06 18:09:03 -0300

    docs: update explanations for reset command

--- src/commands/resolvers/help.ts
+++ src/commands/resolvers/help.ts
@@ -22,7 +22,8 @@ export const helpCommand: CommandStateResolver<'help'> = (
     '/edit --> Permite a você trocar seu nome e/ou sua descrição.\n\n' +
     '/delete --> Permite a você deletar uma conexão ou descadastrar-se da plataforma.\n\n' +
     '/help --> Mostra novamente essa lista. Alternativamente, você pode digitar / e a lista de comandos também aparecerá!\n\n' +
-    'Em qualquer momento você pode resetar o estado do bot mandando /reset\n\n' +
+    'Em qualquer momento você pode resetar o estado do bot mandando /reset.\n' +
+    '⚠️ ATENÇÃO ⚠️   Este comando não irá resetar o seu registro no bot, apenas irá limpar seu estado atual nele para te livrar de eventuais bugs.\n\n' +
     'Caso tenha algum problema ou crítica/sugestão, chama um dos meus desenvolvedores (eles me disseram que não mordem) --> @vitorsanc @Lui_Tombo @arenasoy @Angra018 @OliveiraNelson';
 	/* eslint-enable max-len */
 	client.sendMessage(helpText);
